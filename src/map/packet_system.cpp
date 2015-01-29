d53d65d68430a0c7f35984c9361f2c4c0bc98f4c
LegionXI <mrsentientone@gmail.com>
2015-01-29 01:39:33 +0000

    Proposed changes to exp table to meet SE changes post 99 cap..
    note: this does not include item level as yet.
    
    introduced level check changes as retail i.e level 99 player -> mob level 56 /check incredibly easy prey.

--- src/map/packet_system.cpp
+++ src/map/packet_system.cpp
@@ -4064,10 +4064,11 @@ void SmallPacket0x0DD(map_session_data_t* session, CCharEntity* PChar, int8* dat
             // NOTE: message 0x41: Incredibly Easy Prey
             if (baseExp >= 400) MessageValue = 0x47;
             else if (baseExp >= 240) MessageValue = 0x46;
-            else if (baseExp >= 120) MessageValue = 0x45;
-            else if (baseExp == 100) MessageValue = 0x44;
+            else if (baseExp >= 208) MessageValue = 0x45;
+            else if (baseExp == 200) MessageValue = 0x44;
             else if (baseExp >= 75) MessageValue = 0x43;
             else if (baseExp >= 15) MessageValue = 0x42;
+			else if (baseExp >= 1) MessageValue = 0x41;
             else if (baseExp == 0) MessageValue = 0x40;
             if (mobDef > charAtt && (mobEva - 30) > charAcc)
                 PChar->pushPacket(new CMessageBasicPacket(PChar, PTarget, 0, MessageValue, 170));//high eva high def
